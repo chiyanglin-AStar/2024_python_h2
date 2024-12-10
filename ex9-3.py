90db523affebd07dbd855491f42ac554724859e0
chiyanglin <chiyanglin@gmail.com>
2024-12-10 21:04:38 +0800

    Create ex9-3.py

---
+++ ex9-3.py
@@ -0,0 +1,15 @@
+fruit = {
+    "apple": 1,
+    "banana": 30,
+    "Cherry": 100
+}
+a = fruit["apple"]
+b = fruit["banana"]
+c = fruit["Cherry"]
+d = fruit.keys()
+e = fruit.values()
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print(d)
+print(e)
